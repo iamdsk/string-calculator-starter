570a7ad1ceaa9b146a53746af81f7ae60a1672e9
Swapnil Kondiba Dhanawate <84777131+iamdsk@users.noreply.github.com>
2021-10-17 22:41:49 +0530

    String Calculator Program

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,8 +2,41 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-        return 0;
-    }
+   public int add(String input) 
+		    {
+		        String[] numbers = input.split(",|\n");
+
+		        if (input.isEmpty()) 
+		        {
+		            return 0;
+		        } 
+		        else if (numbers.length > 1) 
+		        {
+		            return getSum(numbers);
+		        }
+		        return stringToIntConversion(input);
+		    }
+
+		    private int getSum(String[] numbers) 
+		    {
+		        int sum = 0;
+		        for (String currentNumber:numbers) {
+		            if (stringToIntConversion(currentNumber) > 1000) {
+		                continue;
+		            }
+		            sum = sum + stringToIntConversion(currentNumber);
+		        }
+		        return sum;
+		    }
+
+		    private int stringToIntConversion(String number) 
+		    {
+		        int num = Integer.parseInt(number);
+		        if (num < 0) {
+		            throw new IllegalArgumentException("The input is negative!");
+		        } else {
+		            return num;
+		        }
+		    }
 
 }
